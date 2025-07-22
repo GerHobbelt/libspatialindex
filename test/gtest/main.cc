f5087dc220b4e389129ae02116bf32ec2e88f87d
Ger Hobbelt <ger@hobbelt.com>
2025-07-22 14:11:24 +0200

    minimal tweak for monolithic build mode: const correctness

--- test/gtest/main.cc
+++ test/gtest/main.cc
@@ -2,7 +2,7 @@
 
 #include "sidx_api_test.h"
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
